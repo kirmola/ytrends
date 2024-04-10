e93cbc27ca92af5c9cc687f5086c7c55d0296eaf
Aman Rawat <ar.rawataman@gmail.com>
2024-04-10 10:02:58 +0530

    fixed title of pages

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import (
-    QuestionDetailView
+    QuestionDetailView,
+    questionfunc,
 )
 from django_distill import distill_path
-from .views import questionfunc
 
 urlpatterns = [
     distill_path("<slug:question_slug>/", QuestionDetailView.as_view(), name="Question_detail", distill_func=questionfunc)
